64bdd5b77e42cc0b846150eee3dbc7605b8892e5
Jeronimo <jeronimodlb.developer@gmail.com>
2023-09-22 17:25:00 -0300

    add new functions

--- app.js
+++ app.js
@@ -23,18 +23,28 @@ function sumarGasto() {
 
 // Funcion para calcular el total de los gastos
 function calcularTotal() {
-  let total = 0;
-  for (let i = 0; i < gastos.length; i++) {
-    total += gastos[i].monto;
-  }
+  const total = gastos.reduce(
+    (acumulador, gasto) => acumulador + gasto.monto,
+    0
+  );
   return total;
+}
+
+// Funcion para mostrar todos los gastos
+function mostrarGastos() {
+  console.log("LISTA DE GASTOS:");
+  gastos.forEach((gasto, index) => {
+    console.log(
+      `Gasto ${index + 1}: ${gasto.descripcion}, Monto: $${gasto.monto}`
+    );
+  });
 }
 
 // Funcion para interactuar con el usuario
 function principal() {
   while (true) {
     const opciones = prompt(
-      "OPCIONES: (1) Agregar un gasto. (2) Calcular el total de gastos. (3) Salir."
+      "OPCIONES: (1) Agregar un gasto. (2) Calcular total de gastos. (3) Mostrar gastos. (4) Salir."
     );
 
     switch (opciones) {
@@ -43,9 +53,12 @@ function principal() {
         break;
       case "2":
         const total = calcularTotal();
-        console.log("Total de gastos: $ " + total);
+        console.log("TOTAL DE GASTOS: $ " + total);
         break;
       case "3":
+        mostrarGastos();
+        break;
+      case "4":
         return;
       default:
         console.log("Opción no válida. Por favor, elija una opción válida.");
